069f0cdef7783979d10767f272a10acf881f0f17
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-05-22 18:55:46 -0300

    create signIn method

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Platform } from "react-native";
+import {
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from "./styles";
+
+export function SignIn() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  function handleSignIn() {}
+
+  function handleRoute() {}
+
+  return (
+    <Background>
+      <Container
+        behavior={Platform.OS === "ios" ? "pading" : undefined}
+        enabled
+      >
+        <Logo source={require("../../assets/logo.png")} />
+
+        <AreaInput>
+          <Input
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={email}
+            onChangeText={(text: string) => setEmail(text)}
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={password}
+            onChangeText={(text: string) => setPassword(text)}
+          />
+        </AreaInput>
+
+        <SubmitButton onPress={handleSignIn}>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link onPress={handleRoute}>
+          <LinkText>Criar uma conta</LinkText>
+        </Link>
+      </Container>
+    </Background>
+  );
+}
